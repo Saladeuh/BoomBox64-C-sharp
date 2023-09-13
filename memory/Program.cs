1fd760d502ae68c9e4b458a5f76e9699e6ea0cd2
Saladeuh <emilienmonet10@gmail.com>
2023-09-13 17:38:00 +0200

    Initial commit

---
+++ memory/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace memory
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+
+      ConsoleKeyInfo keyinfo;
+      do
+      {
+        keyinfo = Console.ReadKey();
+        Console.WriteLine(keyinfo.Key + " was pressed");
+      }
+      while (keyinfo.Key != ConsoleKey.X);
+    }
+  }
+}
