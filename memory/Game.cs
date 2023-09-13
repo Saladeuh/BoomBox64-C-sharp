72ca296b7f11bb9d403c915ec98d7cae45655bd0
Saladeuh <emilienmonet10@gmail.com>
2023-09-13 18:20:22 +0200

    Création de la classe Game et de ses propriétés principales

--- memory/Game.cs
+++ memory/Game.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace memory
+namespace memory;
+
+internal class Game
 {
-  internal class Game
+  private int Retry { get; set; }
+  private int MaxRetry { get; set; }
+  private int[,] Grid { get; set; }
+  public GameStatus Status { get; set; }
+  public Game()
   {
+    MaxRetry = 10;
+    Grid = new int[3,2];
+    FillGridByRandomInt();
+    Status = GameStatus.New;
+  }
+  public void FillGridByRandomInt()
+  {
+   var rnd= new Random();
+    for (int i = 0; i < Grid.GetLength(0); i++)
+    {
+      for (int j = 0; j < Grid.GetLength(1); j++)
+        Grid[i, j] = rnd.Next(1, 3);
+    }
+  }
+  public void Play()
+  {
+    ConsoleKeyInfo keyinfo;
+    do
+    {
+      keyinfo = Console.ReadKey();
+      Console.WriteLine(keyinfo.KeyChar);
+    } while (keyinfo.Key != ConsoleKey.X);
   }
 }
--- memory/Program.cs
+++ memory/Program.cs
@@ -4,14 +4,8 @@
   {
     static void Main(string[] args)
     {
-
-      ConsoleKeyInfo keyinfo;
-      do
-      {
-        keyinfo = Console.ReadKey();
-        Console.WriteLine(keyinfo.Key + " was pressed");
-      }
-      while (keyinfo.Key != ConsoleKey.X);
+      Game game=new Game();
+      game.Play();
     }
   }
 }
